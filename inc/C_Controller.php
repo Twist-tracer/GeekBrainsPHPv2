62b648338f122c48c11240ca8daa3e38eb53c25a
Twist tracer <twist.tracer@gmail.com>
2016-03-31 21:35:36 +0300

    Добавленны методы осуществляющие проверку на REQUEST_METHOD

--- inc/C_Controller.php
+++ inc/C_Controller.php
@@ -1,6 +1,14 @@
 <?php
 abstract class C_Controller {
 
+    protected  function IsGet() {
+        return $_SERVER["REQUEST_METHOD"] == "GET";
+    }
+
+    protected  function IsPost() {
+        return $_SERVER["REQUEST_METHOD"] == "POST";
+    }
+
     protected function Template($file, $params = array()) {
         foreach($params as $k => $v) {
             $$k = $v;
