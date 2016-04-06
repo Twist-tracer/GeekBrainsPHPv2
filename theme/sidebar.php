95b13353ba64d36262463ee9ec0da041fe388544
Twist tracer <twist.tracer@gmail.com>
2016-04-06 06:08:18 +0300

    Добавленна возможность подключения необходимых модулей

--- theme/sidebar.php
+++ theme/sidebar.php
@@ -1,7 +1,3 @@
 <div class="sidebar">
-    <aside class="module">
-        <ul class="reset-list">
-            <li class="icon icon_add reset-list__item"><a href="index.php?c=article&a=new">Добавить статью</a></li>
-        </ul>
-    </aside>
+    <?=$modules?>
 </div>
