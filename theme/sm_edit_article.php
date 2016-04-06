b795a513bf4b0afd0d2627b638a91f31a6a60a5d
Twist tracer <twist.tracer@gmail.com>
2016-04-06 06:09:24 +0300

    Шаблон модуля для работы со статьями

---
+++ theme/sm_edit_article.php
@@ -0,0 +1,5 @@
+<aside class="module module_edit-article">
+    <ul class="reset-list">
+        <li class="icon icon_add reset-list__item"><a href="index.php?c=article&a=new">Добавить статью</a></li>
+    </ul>
+</aside>
