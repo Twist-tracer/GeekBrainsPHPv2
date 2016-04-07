3dbfad4db86968fbee67e2a74df8251c31fffcaa
Twist tracer <twist.tracer@gmail.com>
2016-04-07 07:19:14 +0300

    Исправлен путь до стилей

--- theme/main.php
+++ theme/main.php
@@ -2,7 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="theme/style.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
 
     <title><?=$top_title?></title>
 </head>
