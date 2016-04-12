64e633fd15cb2f754b5b218277fe91fb466b5509
Twist tracer <twist.tracer@gmail.com>
2016-04-12 05:34:21 +0300

    Исправлен путь до контроллеров

--- configs/config.php
+++ configs/config.php
@@ -2,7 +2,7 @@
     function __autoload($name) {
         switch($name[0]) {
             case 'C':
-                include_once("inc/$name.php");
+                include_once("controlers/$name.php");
                 break;
             case 'M':
                 include_once("model/$name.php");
