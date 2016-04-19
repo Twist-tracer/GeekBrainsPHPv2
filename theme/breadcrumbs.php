d650b8a0b82e313936698ab2a7de25b8931e8f55
Twist tracer <twist.tracer@gmail.com>
2016-04-19 20:09:29 +0300

    Ссылки приведены к ЧПУ виду

--- theme/breadcrumbs.php
+++ theme/breadcrumbs.php
@@ -1,7 +1,7 @@
 <div class="breadcrumbs">
     <ul class="crumbs breadcrumbs-list">
         <li class="crumbs__item crumbs__item_first">
-            <a class="crumbs__link crumbs__link_first" href="index.php?c=article&a=editor">Консоль редактора</a>
+            <a class="crumbs__link crumbs__link_first" href="articles/editor">Консоль редактора</a>
         </li>
         <li class="crumbs__item">
             <a class="crumbs__link" href="<?=$link?>"><?=$cont_title?></a>
