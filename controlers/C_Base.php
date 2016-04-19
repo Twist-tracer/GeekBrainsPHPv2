746b3a85eb6d3295adfad3f1b24d56eb138f166c
Twist tracer <twist.tracer@gmail.com>
2016-04-19 21:34:29 +0300

    Реализована проверка прав пользователя на доступ к той или иной странцие. Добавленно подключение файла с настройками сайта. Настройки подключения к БД вынесены в файл настроек (config.php)

--- controlers/C_Base.php
+++ controlers/C_Base.php
@@ -10,14 +10,27 @@ class C_Base extends C_Controller {
     protected $content;
     protected $comments;
     protected $users;
+    protected $config;
 
     public function __construct() {
         $this->top_title = "Главная";
         $this->title = "Добро пожаловать на мой сайт!!!";
         $this->comments = "";
+        $this->config = new Config();
+        $this->users = M_Users::Instance();
+
+        // Проверка прав пользователя на доступ к консоли
+        if(isset($_COOKIE["login"])) {
+            $current_user = $this->users->GetByLogin($_COOKIE["login"]);
+
+            if($this->users->Can("CONSOL_ACCESS", $current_user["id"])) {
+                $consol_access = true;
+            } else $consol_access = false;
+        } else $consol_access = false;
 
         $main_menu = $this->Template("theme/main_menu.php", array(
             "current" => $this->top_title,
+            "consol_access" => $consol_access
         ));
 
         $this->main_menu = $main_menu;
@@ -32,7 +45,6 @@ class C_Base extends C_Controller {
 
         // Устанавливаем дескриптор
         $this->connectDB = $this->startup();
-        $this->users = M_Users::Instance();
     }
 
     public function Render() {
@@ -41,6 +53,7 @@ class C_Base extends C_Controller {
 
         // Основной шаблон->Центральная часть->Вывод статей
        $page = $this->Template("theme/main.php", array(
+           "config" => $this->config,
            "top_title" => $this->top_title,
            "main_menu" => $this->main_menu,
            "title" => $this->title,
@@ -53,16 +66,10 @@ class C_Base extends C_Controller {
 
     // Метод подключения к БД
     private function startup() {
-        // Настройки подключения к БД.
-        $hostname = 'localhost';
-        $username = 'root';
-        $password = '';
-        $dbName = 'GB_PHPv2';
-
         // Подключение к БД.
-        $connect = mysqli_connect($hostname, $username, $password) or die('No connect with data base');
+        $connect = mysqli_connect($this->config->hostname, $this->config->username, $this->config->password) or die('No connect with data base');
         // Выбираем БД, с которой будем работать
-        mysqli_select_db($connect, $dbName) or die('No data base');
+        mysqli_select_db($connect, $this->config->dbName) or die('No data base');
         // Устанавливаем кодировку соединения
         mysqli_query($connect, 'SET NAMES utf8');
 
