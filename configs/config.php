cfc1408bbc7124432d3d2d6adde15ded0c3d2041
Twist tracer <twist.tracer@gmail.com>
2016-04-19 21:14:19 +0300

    Добавлен класс с настройками сайта

--- configs/config.php
+++ configs/config.php
@@ -1,4 +1,14 @@
 <?php
+    class Config {
+        // Корневая папка сайта
+        var $base_url = "/php_lvl2/source/lesson8/HW/";
+        // Настройки подключения к БД
+        var $hostname = 'localhost';
+        var $username = 'root';
+        var $password = '';
+        var $dbName = 'GB_PHPv2';
+    }
+
     function __autoload($name) {
         switch($name[0]) {
             case 'C':
