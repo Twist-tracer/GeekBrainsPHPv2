506dda9484ece847ec8965849b11e643263a4eed
Twist tracer <twist.tracer@gmail.com>
2016-04-19 21:08:11 +0300

    Ссылки приведены к ЧПУ виду

--- theme/main_menu.php
+++ theme/main_menu.php
@@ -1,8 +1,10 @@
 <ul class="main-menu">
     <li class="main-menu__item <?php if ($current == "Главная") print "main-menu__item_current"?>">
-        <a href="index.php?c=article&a=index">Главная</a>
+        <a href="articles/index">Главная</a>
     </li>
-    <li class="main-menu__item <?php if ($current == "Консоль редактора") print "main-menu__item_current"?>">
-        <a href="index.php?c=article&a=editor">Консоль редактора</a>
-    </li>
+    <?php if ($consol_access): ?>
+        <li class="main-menu__item <?php if ($current == "Консоль редактора") print "main-menu__item_current"?>">
+            <a href="articles/editor">Консоль редактора</a>
+        </li>
+    <?php endif ?>
 </ul>
