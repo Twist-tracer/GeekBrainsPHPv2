700c1f5914b63d5e8753a54fb005845882e9f62b
Twist tracer <twist.tracer@gmail.com>
2016-04-19 20:27:44 +0300

    Ссылки приведены к ЧПУ виду

--- theme/sm_edit_article.php
+++ theme/sm_edit_article.php
@@ -1,6 +1,6 @@
 <aside class="module module_edit-article">
     <h2 class="module__title">Статьи</h2><hr>
-    <ul class="reset-list">
-        <li class="icon icon_add reset-list__item"><a href="index.php?c=article&a=new">Добавить статью</a></li>
+    <ul class="list">
+        <li class="icon icon_add list__item"><a href="articles/new">Добавить статью</a></li>
     </ul>
 </aside>
